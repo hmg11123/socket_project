3d97888ecaa2f4c7adfb9709d3163f760a53dc0f
hmg11123 <4leaf.hmg@gmail.com>
2022-10-08 01:17:18 +0900

    modify

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const app = require("express")();
+const express = require("express");
+
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 const PORT = 4000;
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/login.html");
@@ -14,7 +18,7 @@ app.get("/main", (req, res) => {
 
 io.on("connection", (socket) => {
   socket.on("chat", (msg) => {
-    io.emit("chat", msg)
+    io.emit("chat", msg);
   });
 });
 
